170fe497182391c1dd6811533fbb2e9153ed8019
SMCCZ <sabir.mondal@cloudcraftz.io>
2023-02-22 13:30:12 +0530

    CI CD developemnt

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import "./App.css";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import firebaseConfig from "./services/api";
 
 function App() {
   const app = initializeApp(firebaseConfig);
   const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const db = getFirestore(app);
-    const imagesCollection = collection(db, "images");
-    
-    const imagesSnapshot = await getDocs(imagesCollection);
-    const imagesList = imagesSnapshot.docs.map((doc) => doc.data());
-    console.log(imagesList);
-    setImages(imagesList);
-    return imagesList;
-  };
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
@@ -32,45 +21,3 @@ function App() {
 }
 
 export default App;
-const firebaseConfig = {
-  apiKey: "AIzaSyCC6I-1-EdT-3RzwoW5Xfg-gmHLryRRv7I",
-  authDomain: "team-ion-21.firebaseapp.com",
-  projectId: "team-ion-21",
-  storageBucket: "team-ion-21.appspot.com",
-  messagingSenderId: "322547275203",
-  appId: "1:322547275203:web:499f42356d52940db01976",
-};
-
-/*
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-// Initialize Firebase
-
-=============================================
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCC6I-1-EdT-3RzwoW5Xfg-gmHLryRRv7I",
-    authDomain: "team-ion-21.firebaseapp.com",
-    projectId: "team-ion-21",
-    storageBucket: "team-ion-21.appspot.com",
-    messagingSenderId: "322547275203",
-    appId: "1:322547275203:web:499f42356d52940db01976"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
-*/
